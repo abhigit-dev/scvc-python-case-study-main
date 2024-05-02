f4a396b2b113f6406e235c892617cba58decb983
Abhijit Achare <abhijit.achare@betwaygroup.com>
2024-05-02 15:11:39 +0200

    make all methods async

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Customer Profile API")
 
 @app.post("/customers/", response_model=schemas.Customer)
-def create_customer(customer: schemas.CustomerCreate, db: Session = Depends(database.get_db)):
+async def create_customer(customer: schemas.CustomerCreate, db: Session = Depends(database.get_db)):
     db_customer = models.Customer(name=customer.name, email=customer.email, age=customer.age, signup_date=customer.signup_date)
     db.add(db_customer)
     db.commit()
@@ -16,21 +16,21 @@ def create_customer(customer: schemas.CustomerCreate, db: Session = Depends(data
     return db_customer
 
 @app.get("/customers", response_model= list[schemas.Customer])
-def read_all_customer(db: Session = Depends(database.get_db)):
+async def read_all_customer(db: Session = Depends(database.get_db)):
     db_customer = db.query(models.Customer).all()
     if db_customer is None:
         raise HTTPException(status_code=404, detail="Customer not found")
     return db_customer
 
 @app.get("/customers/{customer_id}", response_model= schemas.Customer)
-def read_customer(customer_id: int, db: Session = Depends(database.get_db)):
+async def read_customer(customer_id: int, db: Session = Depends(database.get_db)):
     db_customer = db.query(models.Customer).filter(models.Customer.id == customer_id).first()
     if db_customer is None:
         raise HTTPException(status_code=404, detail="Customer not found")
     return db_customer
 
 @app.put("/customers/{customer_id}", response_model=schemas.Customer)
-def update_customer(customer_id: int, customer: schemas.CustomerUpdate, db: Session = Depends(database.get_db)):
+async def update_customer(customer_id: int, customer: schemas.CustomerUpdate, db: Session = Depends(database.get_db)):
     db_customer = db.query(models.Customer).filter(models.Customer.id == customer_id).first()
     if not db_customer:
         raise HTTPException(status_code=404, detail="Customer not found")
@@ -41,7 +41,7 @@ def update_customer(customer_id: int, customer: schemas.CustomerUpdate, db: Sess
     return db_customer
 
 @app.delete("/customers/{customer_id}", status_code=204)
-def delete_customer(customer_id: int, db: Session = Depends(database.get_db)):
+async def delete_customer(customer_id: int, db: Session = Depends(database.get_db)):
     db_customer = db.query(models.Customer).filter(models.Customer.id == customer_id).first()
     if not db_customer:
         raise HTTPException(status_code=404, detail="Customer not found")
